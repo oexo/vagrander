4503cf0a43f4983d58df4efdb913ffe45f9fc50e
Dmitry Garbovsky <Garbovskiy.D@wb.ru>
2022-01-02 21:43:29 +0300

    add inventory and scheme

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-# This is a sample Python script.
+import os
+from pathlib import Path
+import jsonschema
+from jsonschema import validate
+import yaml
+import json
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+SCHEMA = {
+    "type": "object",
+    "required": ["nodes", "templates"]
+
+}
 
 VAGRANT_CATALOG = "/Users/dmitriygarbovskiy/Learning/vagrant/"
 VAGRANT_TEMPLATE_NAME = "VTemplate.txt"
+MAIN_DIR = os.path.dirname(os.path.realpath(__file__))
+
+with open(Path(MAIN_DIR + "/" + "./inventory.yaml"), "r") as inventory:
+    INVENTORY = yaml.safe_load(inventory)
 
 
 def open_template_file(catalog=VAGRANT_CATALOG, file=VAGRANT_TEMPLATE_NAME):
@@ -19,11 +31,32 @@ def create_vagrant_from_template(hostname, ip):
     #TODO: компилировать вагрант-файл на основе шаблона и инвентори
 
 
+def validate_inventory(inventory):
+    validate(inventory, yaml.safe_load(SCHEMA))
+
+
+def is_json_valid(json_data: dict, json_schema: dict) -> bool:
+    """
+    Func for validate json by the python module jsonschema
+    :param json_data: Dictionary for confirmation
+    :param json_schema: Confirmation scheme
+    :return: Bool, The True if the dict is valid, and False if not.
+    """
+    try:
+        validate(instance=json_data, schema=json_schema)
+    except jsonschema.exceptions.ValidationError as err:
+        return False
+    return True
+
 
 def main():
-    print(open_template_file())
+    # for data in INVENTORY:
+    #     print(INVENTORY[data])
 
-# Press the green button in the gutter to run the script.
+    print(INVENTORY)
+    print(is_json_valid(INVENTORY, SCHEMA))
+
+
 if __name__ == '__main__':
     main()
 
